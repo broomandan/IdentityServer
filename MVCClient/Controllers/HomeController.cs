6259f6f1f6739a1f5d0eafdd4f3a64638ef533b6
Robert Broomandan <robert.broomandan@amerihome.com>
2016-11-17 14:45:36 -0800

    Configured Identity server and a MVC client.
    * Implementation of getting started with Identity server
    * Configured OpenIDConnect authentixation
    * Configured Authorization (basic and custom)

---
+++ MVCClient/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using Thinktecture.IdentityModel.Mvc;
+
+namespace MVCClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Order = 1, Roles = "Developer")] // Authorization option 1- using Authorize
+        [HandleForbidden]
+        public ActionResult About()
+        {
+            ViewBag.Message = "About";
+
+            var user = User as ClaimsPrincipal;
+            var token = user.FindFirst("access_token");
+
+            if (token != null)
+            {
+                ViewData["access_token"] = token.Value;
+            }
+
+            return View();
+
+        }
+        [ResourceAuthorize("Read", "ContactDetails")] // Authorization option 2- using ResourceAuthorize
+        [HandleForbidden]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [ResourceAuthorize("Write", "ContactDetails")] // Authorization option 2- using ResourceAuthorize
+        [HandleForbidden]
+        public ActionResult UpdateContact()
+        {
+            ViewBag.Message = "Your UpdateContact page.";
+
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Request.GetOwinContext().Authentication.SignOut();
+            return Redirect("/");
+        }
+    }
+}
---
+++ MVCClient/Startup.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web.Helpers;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using Sample;
+
+[assembly: OwinStartup(typeof(MVCClient.Startup))]
+
+namespace MVCClient
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = IdentityServer3.Core.Constants.ClaimTypes.Subject;
+            JwtSecurityTokenHandler.InboundClaimTypeMap = new Dictionary<string, string>();
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "Cookies"
+            });
+
+            app.UseResourceAuthorization(new AuthorizationManager());
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+            {
+                Authority = Constants.BaseAddress,
+                ClientId = "robert.mvc",
+                RedirectUri = "https://localhost:44320/",
+                ResponseType = "id_token",
+                Scope = "openid profile roles",
+                SignInAsAuthenticationType = "Cookies",
+                UseTokenLifetime = false,
+                Notifications = new OpenIdConnectAuthenticationNotifications
+                {
+                    SecurityTokenValidated = n =>
+                    {
+                        var id = n.AuthenticationTicket.Identity;
+
+                        // we want to keep first name, last name, subject and roles
+                        var givenName = id.FindFirst(IdentityServer3.Core.Constants.ClaimTypes.GivenName);
+                        var familyName = id.FindFirst(IdentityServer3.Core.Constants.ClaimTypes.FamilyName);
+                        var sub = id.FindFirst(IdentityServer3.Core.Constants.ClaimTypes.Subject);
+                        var roles = id.FindAll(IdentityServer3.Core.Constants.ClaimTypes.Role);
+
+                        // create new identity and set name and role claim type
+                        var nid = new ClaimsIdentity(
+                            id.AuthenticationType,
+                            IdentityServer3.Core.Constants.ClaimTypes.GivenName,
+                            IdentityServer3.Core.Constants.ClaimTypes.Role);
+
+                        nid.AddClaim(givenName);
+                        nid.AddClaim(familyName);
+                        nid.AddClaim(sub);
+                        nid.AddClaims(roles);
+
+                        //Used for post login redirect 
+                        nid.AddClaim(new Claim("id_token", n.ProtocolMessage.IdToken));
+
+                        n.AuthenticationTicket = new AuthenticationTicket(
+                            nid,
+                            n.AuthenticationTicket.Properties);
+
+                        return Task.FromResult(0);
+                    }
+                    ,
+                    RedirectToIdentityProvider = n =>
+                    {
+                        if (n.ProtocolMessage.RequestType == OpenIdConnectRequestType.LogoutRequest)
+                        {
+                            var idTokenHint = n.OwinContext.Authentication.User.FindFirst("id_token");
+
+                            if (idTokenHint != null)
+                            {
+                                n.ProtocolMessage.IdTokenHint = idTokenHint.Value;
+                            }
+                        }
+
+                        return Task.FromResult(0);
+                    }
+                }
+            });
+        }
+    }
+}
